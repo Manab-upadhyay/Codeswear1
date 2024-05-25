7c1a92e990406d6436806c7442b96c57c8a0d1d3
Manab-upadhyay <137534329+Manab-upadhyay@users.noreply.github.com>
2024-05-25 20:33:04 +0530

    fin1

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -120,7 +120,7 @@ console.log("colorset")
     <section className="text-gray-600 body-font overflow-hidden">
   <div className="container px-5 py-24 mx-auto">
     <div className="lg:w-4/5 mx-auto flex flex-wrap">
-      <img alt="ecommerce" className="lg:w-1/2 w-80 lg:h-auto h-50 object-cover object-center rounded" src={apiData?.[slug]?.img}/>
+      <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-50 object-cover object-center rounded" src={apiData?.[slug]?.img}/>
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
         <h2 className="text-sm title-font text-gray-500 tracking-widest">{apiData?.[slug]?.category}</h2>
         <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{apiData?.[slug]?.details} {color} {size}</h1>
